05d6027fda8bd1946046c839defbf4c22374345e
granatumx <dgarmire@umich.edu>
2020-05-10 10:24:00 -0400

    Update

--- src/server/getSingletonKnex.ts
+++ src/server/getSingletonKnex.ts
@@ -8,6 +8,12 @@ export default async () => {
     knex = await Knex({
       client: 'pg',
       connection: config.databaseUrl,
+      pool: {
+	      min: 2, 
+	      max: 100, 
+	      acquireTimeoutMillis: 30000, 
+	      reapIntervalMillis: 1000,
+      },
     });
   }
   return knex;
